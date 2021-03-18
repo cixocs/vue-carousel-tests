07459cf11bf842c75e8eb650f660df6581ef1823
cisco_yoshikawa <exiria7@gmail.com>
2021-03-18 13:32:58 +0900

    json test.

--- src/.vuepress/components/VueCarousel.vue
+++ src/.vuepress/components/VueCarousel.vue
@@ -1,9 +1,18 @@
 <template lang="pug">
 .vue-carousel
   h2.vue-carousel__title(ref='heading') vue-carousel test
+  .vue-carousel__inner
+    Carousel.vue-carousel__block(:perPage='3')
+      Slide.vue-carousel__track(v-for='num in 30', :key='num.id')
+        .vue-carousel__track-image
+          img(:src='carouselItem(num).imagePath', :alt='carouselItem(num).title')
+        p {{ carouselItem(num).title }}
+        p {{ carouselItem(num).description }}
 </template>
 
 <script>
+import CarouselData from '../data/carousel.json';
+
 import Carousel from 'vue-carousel/src/Carousel.vue';
 import Slide from 'vue-carousel/src/Slide.vue';
 
@@ -13,11 +22,31 @@ export default {
     Slide
   },
   data() {
-    return {};
+    return {
+      carouselData: CarouselData
+    };
   },
-  computed: {},
+  computed: {
+    // changeText() {
+    //   return this.textData[this.currentId];
+    // }
+    carouselItem() {
+      return (index) => {
+        return this.carouselData[index - 1];
+      };
+    }
+  },
   mounted() {},
-  methods: {}
+  methods: {
+    // randomTextId() {
+    //   let rnd = 0;
+    //   while (rnd === this.currentId) {
+    //     rnd = Math.floor(Math.random() * this.textData.length);
+    //   }
+    //   this.currentId = rnd;
+    //   console.log(this.currentId, rnd);
+    // }
+  }
 };
 </script>
 
@@ -25,4 +54,11 @@ export default {
 .vue-carousel
   &__title
     text-align: center
+
+  &__track
+    > p
+      text-align: center
+
+  &__rnd
+    margin-top: 50px
 </style>
