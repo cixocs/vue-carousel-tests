27af67405d00f2d2c86646f4bfdb9b201a776806
cisco_yoshikawa <exiria7@gmail.com>
2021-03-26 15:24:54 +0900

    directiveバグるので撲滅した

--- src/.vuepress/components/VueCarousel.vue
+++ src/.vuepress/components/VueCarousel.vue
@@ -10,15 +10,19 @@
     //-     p {{ carouselItem(num).description }}
 
     //- swiper component
-    //- Swiper(:options='swiperOptions')
-    //-   SwiperSlide(v-for='num in 30', :key='num.id')
-    //-     img(:src='carouselItem(num).imagePath', :alt='carouselItem(num).title')
+    Swiper(ref='swiper', :options='swiperOptions', @slideChangeTransitionStart='onSlideChange')
+      SwiperSlide(v-for='data in carouselData', :key='data.id')
+        img(:src='data.imagePath', :alt='data.title')
+    span.swiper-control-prev(@click='prev') prev
+    span.swiper-control-next(@click='next') next
 
-    //- swiper directive
-    div(v-swiper:mySwiper='swiperOptions')
-      ul.swiper-wrapper
-        li.swiper-slide(v-for='num in 30', :key='num.id')
-          img(:src='carouselItem(num).imagePath', :alt='carouselItem(num).title')
+    .swiper-info
+      p {{ currentInfo.title }}
+      p {{ currentInfo.description }}
+
+    .swiper-pagefraction
+      p {{ formatCurrentIndex }}
+      p {{ actives }}
 </template>
 
 <script>
@@ -27,42 +31,67 @@ import CarouselData from '../data/carousel.json';
 // import Carousel from 'vue-carousel/src/Carousel.vue';
 // import Slide from 'vue-carousel/src/Slide.vue';
 
-import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper';
-import 'swiper/css/swiper.css';
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
 
 export default {
   components: {
     // Carousel,
-    // Slide
+    // Slide,
     Swiper,
     SwiperSlide
-  },
-  directives: {
-    swiper: directive
   },
   data() {
     return {
       carouselData: CarouselData,
       swiperOptions: {
         loop: true,
-        slidesPerView: 3
-      }
+        slidesPerView: 'auto',
+        centeredSlides: true
+      },
+      current: 0,
+      actives: 0
     };
   },
   computed: {
-    carouselItem() {
-      return (index) => {
-        return this.carouselData[index - 1];
-      };
+    // carouselItem() {
+    //   return (index) => {
+    //     return this.carouselData[index - 1];
+    //   };
+    // },
+    swiper() {
+      return this.$refs.swiper.$swiper;
+    },
+    currentInfo() {
+      return this.carouselData[this.current];
+    },
+    formatCurrentIndex() {
+      const format = ('00' + (this.current + 1)).slice(-2);
+      return format;
     }
   },
-  mounted() {},
-  methods: {}
+  mounted() {
+    this.actives = this.swiper.activeIndex;
+  },
+  methods: {
+    onSlideChange() {
+      const index = this.swiper.realIndex;
+      this.current = index;
+    },
+    prev() {
+      this.swiper.slidePrev();
+    },
+    next() {
+      this.swiper.slideNext();
+    }
+  }
 };
 </script>
 
 <style lang="stylus" scoped>
 .vue-carousel
+  &__inner
+    margin: 0 get_vw(-20)
+
   &__title
     text-align: center
 
@@ -72,4 +101,9 @@ export default {
 
   &__rnd
     margin-top: 50px
+
+.swiper
+  &-slide
+    width: get_vw(230)
+    margin: 0 get_vw(22)
 </style>
