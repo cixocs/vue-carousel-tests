b97114c5764f992a9d06705ffd6d1018eba01466
cisco_yoshikawa <exiria7@gmail.com>
2021-03-29 02:45:52 +0900

    vue-splide add test.

--- src/.vuepress/components/VueCarousel.vue
+++ src/.vuepress/components/VueCarousel.vue
@@ -10,35 +10,56 @@
     //-     p {{ carouselItem(num).description }}
 
     //- swiper component
-    Swiper(ref='swiper', :options='swiperOptions', @slideChangeTransitionStart='onSlideChange')
-      SwiperSlide(v-for='data in carouselData', :key='data.id')
-        img(:src='data.imagePath', :alt='data.title')
-    span.swiper-control-prev(@click='prev') prev
-    span.swiper-control-next(@click='next') next
+    //- Swiper(ref='swiper', :options='swiperOptions', @slideChangeTransitionStart='onSlideChange')
+    //-   SwiperSlide(v-for='data in carouselData', :key='data.id')
+    //-     img(:src='data.imagePath', :alt='data.title')
+    //- span.swiper-control-prev(@click='prev') prev
+    //- span.swiper-control-next(@click='next') next
 
-    .swiper-info
-      p {{ currentInfo.title }}
-      p {{ currentInfo.description }}
+    //- .swiper-info
+    //-   p {{ currentInfo.title }}
+    //-   p {{ currentInfo.description }}
 
-    .swiper-pagefraction
-      p {{ formatCurrentIndex }}
-      p {{ actives }}
+    //- .swiper-pagefraction
+    //-   p {{ formatCurrentIndex }}
+    //-   p {{ actives }}
+
+    //- splide
+    Splide(:options='splideOptions', @splide:move='splideMove')
+      SplideSlide(v-for='data in carouselData', :key='data.id')
+        span.splide__slide-image
+          img(:src='data.imagePath', :alt='data.title')
+    .splide__info
+      .splide__info-summary
+        p {{ currentInfo.title }}
+        p {{ currentInfo.description }}
+      .splide__info-fraction
+        p {{ formatCurrentIndex }}
+        p {{ carouselData.length }}
 </template>
 
 <script>
 import CarouselData from '../data/carousel.json';
 
+// vue-carousel
 // import Carousel from 'vue-carousel/src/Carousel.vue';
 // import Slide from 'vue-carousel/src/Slide.vue';
 
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
+// vue-awesome-swiper
+// import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
+
+// vue-splide
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import '@splidejs/splide/dist/css/themes/splide-default.min.css';
 
 export default {
   components: {
     // Carousel,
     // Slide,
-    Swiper,
-    SwiperSlide
+    // Swiper,
+    // SwiperSlide,
+    Splide,
+    SplideSlide
   },
   data() {
     return {
@@ -47,6 +68,18 @@ export default {
         loop: true,
         slidesPerView: 'auto',
         centeredSlides: true
+      },
+      splideOptions: {
+        type: 'loop',
+        speed: 600,
+        autoWidth: true,
+        clones: 2,
+        focus: 'center',
+        easing: 'ease',
+        pagination: false,
+        keyboard: false,
+        flickPower: 0,
+        updateOnMove: true
       },
       current: 0,
       actives: 0
@@ -58,9 +91,9 @@ export default {
     //     return this.carouselData[index - 1];
     //   };
     // },
-    swiper() {
-      return this.$refs.swiper.$swiper;
-    },
+    // swiper() {
+    //   return this.$refs.swiper.$swiper;
+    // },
     currentInfo() {
       return this.carouselData[this.current];
     },
@@ -70,18 +103,23 @@ export default {
     }
   },
   mounted() {
-    this.actives = this.swiper.activeIndex;
+    // this.actives = this.swiper.activeIndex;
+    console.log(this.actives);
   },
   methods: {
-    onSlideChange() {
-      const index = this.swiper.realIndex;
-      this.current = index;
-    },
-    prev() {
-      this.swiper.slidePrev();
-    },
-    next() {
-      this.swiper.slideNext();
+    // onSlideChange() {
+    //   const index = this.swiper.realIndex;
+    //   this.current = index;
+    // },
+    // prev() {
+    //   this.swiper.slidePrev();
+    // },
+    // next() {
+    //   this.swiper.slideNext();
+    // }
+    splideMove(splide, newIndex) {
+      // console.log(newIndex, oldIndex, destIndex);
+      this.current = newIndex;
     }
   }
 };
@@ -102,8 +140,24 @@ export default {
   &__rnd
     margin-top: 50px
 
+// swiper
 .swiper
   &-slide
     width: get_vw(230)
     margin: 0 get_vw(22)
+
+// splide
+.splide
+  &__slide
+    width: get_vw(270)
+    margin: 0 get_vw(2)
+
+    > span
+      display: block
+      transform: scale3d(0.85, 0.85, 1) rotate(0.001deg)
+      will-change: transform
+      transition: transform 0.7s ease
+
+    &.is-active > span
+      transform: scale3d(1, 1, 1) rotate(0.001deg)
 </style>
